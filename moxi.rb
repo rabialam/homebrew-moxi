9bf7ecf5a73b4892258e78bce0adfeb838fb98c5
Rabi Alam <statokinetics@gmail.com>
2013-02-25 19:36:31 -0500

    adding updated couchbase git repo for moxi

--- moxi.rb
+++ moxi.rb
@@ -1,15 +1,16 @@
 require 'formula'
 
 class Moxi <Formula
-  url 'http://labs.northscale.com/moxi/moxi-0.10.0.tar.gz'
-  homepage 'http://labs.northscale.com/moxi/'
-  md5 'ec73c7521324321eb4c8b4f51f9d7373'
+  head 'https://github.com/couchbase/moxi.git', :using => :git
+  homepage 'https://github.com/couchbase/moxi'
+  md5 '4b391021af7a453bf88716724d2c644916ebd969'
 
  depends_on 'check'
 
   def install
     paths = %W[#{HOMEBREW_PREFIX}/lib/pkgconfig /usr/local/lib/pkgconfig /usr/lib/pkgconfig /usr/X11/lib/pkgconfig].uniq
     ENV['PKG_CONFIG_PATH'] = paths.join(":")
+    system "./config/autorun.sh"
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
   end
